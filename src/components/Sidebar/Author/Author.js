d64fd7f2ce7e29c5fc268977423288c429e4d50e
Lucas Della Bella <dellabella.lucas@gmail.com>
2019-05-12 11:53:34 -0700

    Add weird nametag thing

--- src/components/Sidebar/Author/Author.js
+++ src/components/Sidebar/Author/Author.js
@@ -12,25 +12,41 @@ type Props = {
   isIndex: ?boolean
 };
 
+const Nametag = () => {
+  return (
+    <span className={styles['author__title-nametag']}>
+      lucas della bella 
+      <br />
+      /
+    </span>
+  )
+}
+
 const Author = ({ author, isIndex }: Props) => (
   <div className={styles['author']}>
     <Link to="/">
       <img
         src={withPrefix(author.photo)}
         className={styles['author__photo']}
-        width="75"
-        height="75"
+        width="125"
+        height="125"
         alt={author.name}
       />
     </Link>
 
     { isIndex ? (
       <h1 className={styles['author__title']}>
-        <Link className={styles['author__title-link']} to="/">{author.name}</Link>
+        <Link className={styles['author__title-link']} to="/">
+          Thoughts
+          <Nametag />
+        </Link>
       </h1>
     ) : (
       <h2 className={styles['author__title']}>
-        <Link className={styles['author__title-link']} to="/">{author.name}</Link>
+        <Link className={styles['author__title-link']} to="/">
+          Thoughts
+          <Nametag />
+        </Link>
       </h2>
     )}
     <p className={styles['author__subtitle']}>{author.bio}<br/>😤😤</p>
